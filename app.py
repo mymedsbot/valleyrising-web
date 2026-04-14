17747d9ea2a98897ef59ceb31c4784b1f492ed8c
Gregory McGregor <gregorymcgregor@Gregorys-Mac-mini.local>
2026-04-14 10:19:00 +1000

    Add 'What We Are About' page with philosophy, vision & mission

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def new_patient():
 def contact():
     return render_template('contact.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
